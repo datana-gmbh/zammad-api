1e4fdf24cca89aefb355bb2e710ddcafa4108c20
Oskar Stark <oskarstark@googlemail.com>
2022-03-21 10:07:41 +0100

    Enhancement: Add search endpoint

--- src/TicketsApiInterface.php
+++ src/TicketsApiInterface.php
@@ -18,4 +18,11 @@ use Datana\Zammad\Api\Domain\Value\Ticket;
 interface TicketsApiInterface
 {
     public function create(Ticket $ticket): bool;
-}
+
+    /**
+     * @param string  $searchterm The searchterm to search for.
+     * @param null|int $page                 Page of objects, optional, if given, $objects_per_page must also be given.
+     * @param null|int $objectsPerPage     Number of objects per page, optional, if given, $page must also be given.
+     */
+    public function search(string $searchterm, ?int $page = null, ?int $objectsPerPage = null): array;
+
